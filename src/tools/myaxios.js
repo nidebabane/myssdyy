ac6a38874c53c1de6c91f9e0fb850651a93296d5
nidebabane <2236246159@qq.com>
2019-11-22 20:35:53 +0800

    one

---
+++ src/tools/myaxios.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import QS from 'qs';
+//打包apk时加上
+// axios.defaults.baseURL = "http://127.0.0.1:8899";
+//设置超时时间
+axios.defaults.timeout = 10000;
+// post请求头
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+
+//请求方法的封装
+export default function myaxios(method,url,params){
+    if(method == "GET"){
+        return axios.get(url,{params:params})
+    }
+    else if(method == "POST"){
+        var params = QS.stringify(params);
+        return axios.post(url,params)
+    }
+}
+
+export const searchonelistURL = "/api/getonelist";          //get
+export const searchtuijianURL = "/api/getmovie";          //get
+export const searchmovielistURL = "/api/gettidmovie/";          //get    :id
+export const searchlistURL = "/api/searchmovie/";          //get   :name
+export const searchhotlistURL = "/api/gethotmovie";          //get
